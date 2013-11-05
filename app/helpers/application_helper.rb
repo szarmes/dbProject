d306df6771fef1fda4a40adedb5bfa123a107194
Duncan Szarmes <szarmes@gmail.com>
2013-11-04 17:26:22 -0800

    transferred your_deck view to recent/saved. nice work matt

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Flashcards"
+    base_title = "SCFlashcards"
     if page_title.empty?
       base_title
     else
