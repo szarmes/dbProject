444c19513e823fe28fe72378652f5147b004363b
Matthew Bojey <mbojey@gmail.com>
2013-11-07 23:35:48 -0800

    Got sorting results working, looks bad and needs CSS but works

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,18 +4,44 @@ class SearchController < ApplicationController
   	@subSearch = Deck.new
     @results = Result.all.paginate(page: params[:page])
     @result = Result.new
+    @thing = Deck.new
   end
 
   def index
     @subSearch = Deck.new
+    @thing = Result.new
     @results = Result.all.paginate(page: params[:page])
-    @result = Result.new
   end
 
   def show
+    @thing = Result.new
+    @subSearch = Deck.new
+    @results = Result.all.paginate(page: params[:page])
+  end
+
+  def sort
     @subSearch = Deck.new
     @results = Result.all.paginate(page: params[:page])
     @result = Result.new
+    length = subject_name.to_s.length
+    length -= 19
+    @value = subject_name.to_s[17,length]
+    if(@value == "Rating")
+      @results = Result.order(percent: :desc).paginate(page: params[:page])
+    elsif(@value == "Date Created")
+      @results = Result.order(created_on: :desc).paginate(page: params[:page])
+    elsif(@value == "Author A-Z")
+      @results = Result.order(username: :desc).paginate(page: params[:page])
+    elsif(@value == "Author Z-A")
+      @results = Result.order(username: :asc).paginate(page: params[:page])
+    end
+    @results.each do |r|
+        @result = Result.create(:deck_id => (r.deck_id), :username => (r.username), :percent => r.percent, :created_on => r.created_on)
+        @id = @result.id
+        Result.where(:deck_id => (r.deck_id), :username => (r.username), :percent => r.percent).where.not(:id => @id).delete_all
+        @result.save
+      end
+    redirect_to '/index'
   end
 
   
@@ -35,7 +61,15 @@ class SearchController < ApplicationController
     @nameString = course_num.to_s[15,length]
     @results = Deck.search(@subjectString, @courseString, @nameString)
     @results.each do |r|
-      @result = Result.create(:deck_id => (r.deck_id))
+      if Deckrating.where(deck_id: r.deck_id).empty?
+        @percent = 0
+      else
+        @liked = Deckrating.where(deck_id: r.deck_id, liked: true).count 
+        @total = Deckrating.where(deck_id: r.deck_id).count 
+        @percent = (@liked * 100 / @total) 
+      end
+      @username = User.find_by(:user_id => r.user_id).username
+      @result = Result.create(:deck_id => (r.deck_id), :username => (r.user_id), :percent => @percent, :created_on => r.created_on)
       @result.save
     end
     @results = Result.all.paginate(page: params[:page])
@@ -53,6 +87,9 @@ class SearchController < ApplicationController
   end
   def course_num
     params.require(:deck).permit(:courseNum)
+  end
+  def value
+    params.require(:deck).permit(:value)
   end
 
 
